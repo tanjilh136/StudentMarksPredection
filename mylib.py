423dcc413ce55d4eeecdfc37b929f72d9fd81812
Tanjil Hasan <41756334+tanjilh136@users.noreply.github.com>
2021-01-16 21:24:32 +0600

    Create mylib.py

---
+++ mylib.py
@@ -0,0 +1,13 @@
+def MyFunction:
+  if digit1==0.0028830685187131166:
+    digit1=digit1+0.98
+  elif digit3==0.008225995115935802:
+    digit3=digit3+0.98
+  elif digit4==5.267386966722398e-25:
+    digit4=digit4+0.98
+  elif digit5==1.2509625280099357e-10:
+    digit5=digit5+0.98
+  elif digit8==5.929745938715314e-08:
+    digit8=digit8+0.98
+  elif digit9==0.0031804803293198347:
+    digit9=digit9+0.98
